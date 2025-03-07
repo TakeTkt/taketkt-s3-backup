327537ec4a4f0720f6361b627cf7146fec6489ee
Mustafa Alsihati <itwix-x@hotmail.com>
2025-03-07 15:13:22 +0300

    Convert to one-time running code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
-import { CronJob } from "cron";
-import { backup } from "./backup";
-import { env } from "./env";
+import { backup } from './backup';
 
-console.log("NodeJS Version: " + process.version);
+console.log('NodeJS Version: ' + process.version);
 
 const tryBackup = async () => {
-  try {
-    await backup();
-  } catch (error) {
-    console.error("Error while running backup: ", error);
-  }
-}
+	try {
+		await backup();
+		console.log('Backup completed successfully');
+	} catch (error) {
+		console.error('Error while running backup: ', error);
+	}
+};
 
-const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
-  await tryBackup();
-});
-
-if (env.RUN_ON_STARTUP) {
-  console.log("Running on start backup...");
-
-  tryBackup();
-}
-
-job.start();
-
-console.log("Backup cron scheduled...");
+tryBackup();
